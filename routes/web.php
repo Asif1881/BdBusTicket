d2494c0cd3549b9768d0f6eaf9e45c5422428629
furiousnur <furiousnur@gmail.com>
2020-02-26 23:23:54 +0600

    admin page and route create

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Auth::routes();
 Route::get('/', 'FrontendController@index')->name('bd-bus.index');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/admin', function () {
+    return view('admin');
+});
